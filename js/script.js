cf35d9a560bc4ba97db5b6f9b518316a41e199ef
Rafał Jabłoński <asserk.codes@gmail.com>
2022-11-07 15:49:31 +0100

    new functions added

--- js/script.js
+++ js/script.js
@@ -1,21 +1,27 @@
 {
     console.log("Hello World");
 
+    const changeBackgroundTableHeader = () => {
+        const tableHeader = document.querySelectorAll(".js-tableHeader");
+
+        for (let i = 0; i < tableHeader.length; i++) {
+            tableHeader[i].classList.toggle("table__header--whiteBackground");
+        }
+    };
+
+    const changeBackground = () => {
+        const container = document.querySelector(".js-container");
+        const nextMode = document.querySelector(".js-nextMode");
+
+        container.classList.toggle("container--whiteBackground");
+        nextMode.innerText = container.classList.contains("container--whiteBackground") ? "ciemny" : "jasny";
+        changeBackgroundTableHeader();
+    };
+
     const init = () => {
         const button = document.querySelector(".js-button");
 
-        button.addEventListener("click", () => {
-            const container = document.querySelector(".js-container");
-            const nextMode = document.querySelector(".js-nextMode");
-            const tableHeader = document.querySelectorAll(".js-tableHeader");
-
-            container.classList.toggle("container--whiteBackground");
-            nextMode.innerText = container.classList.contains("container--whiteBackground") ? "ciemny" : "jasny";
-
-            for (let i = 0; i < tableHeader.length; i++) {
-                tableHeader[i].classList.toggle("table__header--whiteBackground");
-            }
-        });
+        button.addEventListener("click", changeBackground);
     };
 
     init();
